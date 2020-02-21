b7b9dd1f86fd02b1bf84626b94b725f557e6cd2f
Bil Benhamou <bil.benhamou@gmail.com>
2020-02-21 18:56:04 +0100

    chore: add support for styled-components

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,28 @@
-import typescript from 'rollup-plugin-typescript2'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
+import babel from "rollup-plugin-babel";
+import typescript from "rollup-plugin-typescript2";
+import commonjs from "rollup-plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
 // import postcss from 'rollup-plugin-postcss-modules'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
+import postcss from "rollup-plugin-postcss";
+import resolve from "rollup-plugin-node-resolve";
+import url from "rollup-plugin-url";
+import svgr from "@svgr/rollup";
 
-import pkg from './package.json'
+import pkg from "./package.json";
 
 export default {
-  input: 'src/index.tsx',
+  input: "src/index.tsx",
   output: [
     {
       file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
+      format: "cjs",
+      exports: "named",
       sourcemap: true
     },
     {
       file: pkg.module,
-      format: 'es',
-      exports: 'named',
+      format: "es",
+      exports: "named",
       sourcemap: true
     }
   ],
@@ -32,11 +33,20 @@ export default {
     }),
     url(),
     svgr(),
+    babel({
+      exclude: "node_modules/**",
+      plugins: ["external-helpers"]
+    }),
     resolve(),
     typescript({
       rollupCommonJSResolveHack: true,
       clean: true
     }),
-    commonjs()
+    commonjs({
+      include: "node_modules/**",
+      namedExports: {
+        "node_modules/react-is/index.js": ["isElement", "isValidElementType"]
+      }
+    })
   ]
-}
+};
